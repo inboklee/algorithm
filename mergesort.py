fb677621c0b5b519de15b95157ea79fa0dad68d2
Inbok Lee <inboklee@users.noreply.github.com>
2015-09-10 13:30:59 +0900

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,11 +1,8 @@
 def merge(A, B):
-    C = []
     if (len(A) == 0):
-        C = list(B)
-        return C
+        return B
     if (len(B) == 0):
-        C = list(A)
-        return C
+        return A
     if (A[0] < B[0]):
         return [A[0]] + merge(A[1:], B)
     else:

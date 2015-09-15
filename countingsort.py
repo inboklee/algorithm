752224196bd2fe4467cb921dc6c6dd7c4258d6a2
Inbok Lee <inboklee@users.noreply.github.com>
2015-09-15 13:19:19 +0900

    Create countingsort.py

---
+++ countingsort.py
@@ -0,0 +1,14 @@
+def countingsort(L, n):
+        n = n + 1
+        C = [0 for _ in xrange(n)]
+        for l in L:
+                C[l] = C[l] + 1
+        for i in xrange(1, n):
+                C[i] = C[i-1] + C[i]
+
+        C = [0] + C[:-1]
+        for i in xrange(1, n):
+                if (C[i] > C[i-1]):
+                        for j in xrange(C[i-1], C[i]):
+                                L[j] = i-1
+        return L

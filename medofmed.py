e03e8cd3adba56efd7ccafedc6a3347c94bad464
Inbok Lee <inboklee@users.noreply.github.com>
2015-09-21 11:31:18 +0900

    Update medofmed.py
    Fixed medofmed(L) by mutual recursion
    
    selectk() is not the same with selectk.py.

--- medofmed.py
+++ medofmed.py
@@ -1,3 +1,22 @@
+def selectk(L, k):
+        if (len(L) == 1):
+                return L[0]
+
+        pivot = medofmed(L)
+        left = []
+        right = []
+        for x in L[1:]:
+                if (x < pivot):
+                        left.append(x)
+                else:
+                        right.append(x)
+        if (len(left) ==  k-1):
+                return pivot
+        elif (len(left) >= k):
+                return selectk(left, k)
+        else:
+                return selectk(right, k-(len(left)+1))
+
 def medofmed(L):
         if (len(L) <= 5):
                 return sorted(L)[len(L)/2]
@@ -6,4 +25,4 @@ def medofmed(L):
         while (current < len(L)):
                 med.append(medofmed(L[current:current+5]))
                 current = current + 5
-        return medofmed(med)
+        return selectk(med, int(len(med)/2))

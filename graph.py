67e76bb5b8da49399de9a4ec36172b93660b8249
Inbok Lee <inboklee@users.noreply.github.com>
2015-11-02 13:12:28 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -12,3 +12,6 @@ def recursiveDFS(G, isMarked, start):
         for e in G[start]:
             recursiveDFS(G, isMarked, e)
         return
+G = [[], [2,6,8], [1,3,6], [2,4], [3,5], [2,3], [1,2,7,8], [6], [1,6]]
+
+DFS(G, 1)
